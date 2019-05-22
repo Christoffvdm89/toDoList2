a81c75565c9abb34456249cb5fe09d4d841e6f7c
Christoffvdm89 <christoffvdm89@gmail.com>
2019-05-22 15:50:15 +0200

    second commit

--- index.php
+++ index.php
@@ -1,31 +1,70 @@
 <?php
-// Starting session
-session_start();
+
+$con = mysqli_connect('localhost','root','','todo');
+// check connection
+if (!$con)
+  {
+  die ("Connection error: " . mysqli_connect_error());
+  }
+
+if(isset($_POST['submit'])){  
+$task = $_POST['task']; 
+$sql = "INSERT INTO tasks (task) VALUES('$task')";
+
+if (mysqli_query($con, $sql)){
+  echo "Record inserted successfully";
+}else{
+  echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+header("location: index.php");
+}
+
+
+
 ?>
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo list application with PHP and MySQL</title>
+    <link rel="stylesheet" href="">
+</head>
 <body>
+    <div class="heading">
+    <h1>Todo list application with PHP and MySQL</h1>
+    </div>
 
+    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post" id="form">  
+         <input type="text" name="task">
+         <button type="submit" name="submit">Add Task</button>
 
-<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post" id="form">
+     </form>
 
-<input type="text" name="task">
-<button type="submit" name="submit">Submit</button>
+     <table>
+       <thead>
+         <tr>
+          <th>N</th>
+          <th>Task</th>
+          <th>Action</th>
+         </tr>
+       </thead>
 
-</form>
-<?php
-
-$_SESSION["task"]= $_POST["task"];
-
-echo $_SESSION["task"];
-
-
-
-
-?>
-
+       
+       <tbody>
+         <tr>
+           <td>1</td>
+           <td>This is the first task placeholder</td>
+           <td>
+             <a href="#">x</a>
+           </td>
+         </tr>
+       </tbody>
+     </table>
+       
 
 </body>
 </html>
